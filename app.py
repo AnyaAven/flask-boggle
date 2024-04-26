e424eb4254fd91882a3ad55d2942efafeb7cc3aa
Anya Aven <leafeon96@gmail.com>
2024-04-26 18:24:56 -0400

    Added debugger toolbar

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, request, render_template, jsonify
 from uuid import uuid4
+from flask_debugtoolbar import DebugToolbarExtension
+
 
 from boggle import BoggleGame
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "this-is-secret"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
 
 # The boggle games created, keyed by game id
 games = {}
